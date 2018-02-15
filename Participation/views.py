9b771c6db6da66c631526451136d14809f013a56
Rohan Malik <malikmalik29@outlook.com>
2018-02-15 16:34:21 +0530

    Events Registration Complete

--- Participation/views.py
+++ Participation/views.py
@@ -8,17 +8,25 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def participate_home(request):
     part_obj, new_obj = Participate.objects.new_or_get(request)
-    return render(request, "Participation/index.html", {})
+    context = {
+        "participation": part_obj
+    }
+    return render(request, "Participation/index.html", context)
 
 
 @login_required(login_url='/accounts/login/')
 def participate_update(request):
-    obj = Event.objects.get(id=2)
-    part_obj, new_obj = Participate.objects.new_or_get(request)
-    if obj in part_obj.eventspart.all():
-        part_obj.eventspart.remove(obj)
-    else:
-        part_obj.eventspart.add(obj)
+    event_id = request.POST.get('event_id')
+    if event_id is not None:
+        try:
+            obj = Event.objects.get(id=event_id)
+        except Event.DoesNotExist:
+            return redirect("Participation:home")
+        part_obj, new_obj = Participate.objects.new_or_get(request)
+        if obj in part_obj.eventspart.all():
+            part_obj.eventspart.remove(obj)
+        else:
+            part_obj.eventspart.add(obj)
     return redirect("Participation:home")
 
 
